401b5cb47f06c6aadae05681755c01957bacc09c
Adnane Ahroum <120498252+Adnane-Ahroum@users.noreply.github.com>
2025-05-19 21:45:28 +0000

    push

--- src/algorithms/__init__.py
+++ src/algorithms/__init__.py
@@ -4,3 +4,22 @@ from .dijkstra import main as dijkstra
 from .bfs import main as bfs
 from .dfs import main as dfs
 from .floyd_warshall import main as floyd_warshall
+
+ALGORITHMS = {
+    "Kruskal": kruskal,
+    "Prim": prim,
+    "Dijkstra": dijkstra,
+    "BFS": bfs,
+    "DFS": dfs,
+    "Floyd-Warshall": floyd_warshall,
+}
+
+__all__ = [
+    "kruskal",
+    "prim",
+    "dijkstra",
+    "bfs",
+    "dfs",
+    "floyd_warshall",
+    "ALGORITHMS",
+]
